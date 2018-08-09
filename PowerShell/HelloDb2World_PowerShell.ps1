cb3c5c12268923b8aa6145a951a8e18bd3842b3e
ecrooks <ember.crooks@gmail.com>
2018-08-08 21:18:26 -0600

    Initial commit - powerShell

---
+++ PowerShell/HelloDb2World_PowerShell.ps1
@@ -0,0 +1,19 @@
+﻿# Source File with connection variables set
+$Path = $PSScriptRoot
+. "$Path\HelloDb2World_PowerShell_variables.ps1"
+
+#Define connection string for the database
+$cn = new-object system.data.OleDb.OleDbConnection("Provider=IBMDADB2;DSN=$dbName;User Id=;Password=;");
+#Define data set for first query
+$ds = new-object "System.Data.DataSet" "ds"
+#Define query to run
+$q = "select * from hello_world"
+# Define data object given the specific query and connection string
+$da = new-object "System.Data.OleDb.OleDbDataAdapter" ($q, $cn)
+# Fill the data set - essentially run the query. 
+$da.Fill($ds) | Out-Null
+# Print the result
+foreach ($Row in $ds.Tables[0].Rows)
+        {
+        write-host  "$($Row.C1)"  
+        }
---
+++ PowerShell/HelloDb2World_PowerShell_variables.ps1
@@ -0,0 +1,6 @@
+﻿$dbName="SAMPLE"
+$userID="db2admin"
+$PW="db2admin"
+$server="localhost"
+$port=50000
+
